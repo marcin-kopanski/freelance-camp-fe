ab48c42789cb81e7241303f4bc34a4ea5d1a5e1e
Marcin Kopanski <marcin.kopanski@gmail.com>
2017-10-06 16:21:59 +0200

    Mockup data for proposals and rendering proposal list

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Proposal } from './proposal';
+
 @Component({
   moduleId: module.id,
   selector: 'proposal-list',
@@ -8,4 +10,11 @@ import { Component } from '@angular/core';
 export class ProposalListComponent {
   pageTitle: string = "Proposals Dashboard";
 
+  proposalOne: Proposal = new Proposal(15, 'ABC Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com');
+  proposalTwo: Proposal = new Proposal(16, 'XYZ Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com');
+  proposalThree: Proposal = new Proposal(17, 'QWE Company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com');
+
+  proposals: Proposal[] = [
+    this.proposalOne, this.proposalTwo, this.proposalThree
+  ]
 }
