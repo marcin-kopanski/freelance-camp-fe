61c3194b669c2abbe97cb9f92860eec274fbe50b
Marcin Kopanski <mk@pwrs.pl>
2017-10-06 13:14:31 +0200

    Hello Angular - first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomepageComponent]
 })
 export class AppModule { }
