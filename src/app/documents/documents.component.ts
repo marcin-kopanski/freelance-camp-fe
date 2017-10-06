b03f7790eebf75c9386ee5565e4b779077fb23a9
Marcin Kopanski <marcin.kopanski@gmail.com>
2017-10-06 15:28:13 +0200

    Mock data inside of documents component

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+import { Document } from './document';
+
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
-
+  documents: Document[] = [
+    {
+      title: "My first doc",
+      description: "Description test",
+      file_url: "http://via.placeholder.com/350x150",
+      updated_at: "11/11/16",
+      image_url: "http://via.placeholder.com/350x150"
+    },
+    {
+      title: "My second doc",
+      description: "Description test",
+      file_url: "http://via.placeholder.com/350x150",
+      updated_at: "11/11/16",
+      image_url: "http://via.placeholder.com/350x150"
+    },
+    {
+      title: "My third doc",
+      description: "Description test",
+      file_url: "http://via.placeholder.com/350x150",
+      updated_at: "11/11/16",
+      image_url: "http://via.placeholder.com/350x150"
+    }
+  ];
 }
