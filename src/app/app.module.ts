ce0a69e72ad241e727e25ac409a7b6c26463ac1a
Marcin Kopański <marcin.kopanski@gmail.com>
2017-10-08 01:35:21 +0200

    Added ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
